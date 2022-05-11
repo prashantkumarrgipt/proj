3277914f5e74a994ee1e0cf3be9815746af15a92
prashantkumarrgipt <99869977+prashantkumarrgipt@users.noreply.github.com>
2022-05-11 16:18:44 +0530

    edit

--- shantvi_django/settings.py
+++ shantvi_django/settings.py
@@ -172,16 +172,16 @@ STATICFILES_FINDERS = (
 
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation (I removed them in the local settings)
 AUTH_PASSWORD_VALIDATORS = [
@@ -204,6 +204,7 @@ SESSION_COOKIE_SECURE = True
 
 # Activate Django-heroku.
 django_heroku.settings(locals())
-# import dj_database_url 
+import dj_database_url 
 
-# db_from_env = dj_database_url.config(conn_max_age = 600)
+db_from_env = dj_database_url.config(conn_max_age = 600)
+DATABASES['default'].update(db_from_env)
