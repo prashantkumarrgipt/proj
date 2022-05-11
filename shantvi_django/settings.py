7f6eb20fbc04b12075cba8c917b6cb5f767a8bd1
prashantkumarrgipt <99869977+prashantkumarrgipt@users.noreply.github.com>
2022-05-11 15:11:57 +0530

    edit

--- shantvi_django/settings.py
+++ shantvi_django/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-#o+b8)%c(tkhay*beb#b&n_=#xcp10cndn^tj99u^z4xezk)(w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 
