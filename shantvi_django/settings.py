6c121a8a1fe5e06e578162a30ee629a7bd7ff4f1
prashantkumarrgipt <99869977+prashantkumarrgipt@users.noreply.github.com>
2022-05-11 15:20:20 +0530

    edit

--- shantvi_django/settings.py
+++ shantvi_django/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#o+b8)%c(tkhay*beb#b&n_=#xcp10cndn^tj99u^z4xezk)(w'
+# SECRET_KEY = 'django-insecure-#o+b8)%c(tkhay*beb#b&n_=#xcp10cndn^tj99u^z4xezk)(w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
